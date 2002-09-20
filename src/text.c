5e72038f78805de0d05bf2e892b9268dc798b63a
dfx <>
2002-09-20 20:31:54 +0000

    libtext config

--- src/text.c
+++ src/text.c
@@ -12,6 +12,18 @@
 
 #include "font_6x11.h"
 
+struct text_ctx
+{
+	unsigned char *text;
+	unsigned char color[3];
+	unsigned char bgcolor[3];
+	int trans:1;
+};
+
+static int text_conf(struct text_ctx *, xmlNodePtr);
+static int text_color(unsigned char *, unsigned char *);
+static int text_hexdig(unsigned char);
+
 #define MODNAME "text"
 
 char *name = MODNAME;
@@ -19,56 +31,132 @@ char *name = MODNAME;
 int
 filter(struct image *img, xmlNodePtr node)
 {
-	unsigned char *text;
+	struct text_ctx ctx;
 	int x, y;
 	int subx, suby;
 	int idx;
 	unsigned char *imgptr;
 	
-	text = NULL;
-
-	for (node = node->children; node; node = node->next)
-	{
-		if (xml_isnode(node, "text"))
-			text = xml_getcontent(node);
-	}
-	if (!text)
-	{
-		log_log(MODNAME, "No \"text\" tag defined\n");
+	idx = text_conf(&ctx, node);
+	if (idx)
 		return -1;
-	}
 	
 	x = 0;
 	y = img->y - 11;
 	
-	while (*text)
+	while (*ctx.text)
 	{
-		printf("letter %c\n", *text);
-		idx = *text * 11;
-		printf("index %i\n", idx);
-		printf("fontdata %x\n", fontdata[idx]);
+		idx = *ctx.text * 11;
 		for (suby = 0; suby < 11; suby++)
 		{
-			printf("  suby %i\n", suby);
 			imgptr = img->buf + ((y + suby) * img->x + x) * 3;
-			printf("  ptr diff %i\n", imgptr - img->buf);
-			for (subx = 0; subx < 8; subx++)
+			for (subx = 0; subx < 6; subx++)
 			{
-				printf("    subx %i\n", subx);
-				printf("    fontdata %x\n", fontdata[idx + suby]);
-				printf("    comp %x\n", (0x80 >> subx));
 				if (fontdata[idx] & (0x80 >> subx))
-					imgptr[0] = imgptr[1] = imgptr[2] = 0xff;
+					memcpy(imgptr, ctx.color, 3);
 				else
-					imgptr[0] = imgptr[1] = imgptr[2] = 0x00;
+				{
+					if (!ctx.trans)
+						memcpy(imgptr, ctx.bgcolor, 3);
+				}
 				imgptr += 3;
 			}
 			idx++;
 		}
-		text++;
-		x += 8;
+		ctx.text++;
+		x += 6;
 	}
 	
 	return 0;
+}
+
+static
+int
+text_conf(struct text_ctx *ctx, xmlNodePtr node)
+{
+	int ret;
+	unsigned char *text;
+	
+	memset(ctx, 0, sizeof(*ctx));
+	memset(ctx->color, 0xff, sizeof(ctx->color));
+	ctx->trans = 1;
+	
+	for (node = node->children; node; node = node->next)
+	{
+		if (xml_isnode(node, "text"))
+			ctx->text = xml_getcontent(node);
+		else if (xml_isnode(node, "color"))
+		{
+			ret = text_color(ctx->color, xml_getcontent(node));
+			if (ret)
+			{
+				log_log(MODNAME, "Invalid contents of <color> tag\n");
+				return -1;
+			}
+		}
+		else if (xml_isnode(node, "bgcolor") || xml_isnode(node, "background"))
+		{
+			text = xml_getcontent(node);
+			if (!text)
+			{
+invbg:
+				log_log(MODNAME, "Invalid contents of <bgcolor> tag\n");
+				return -1;
+			}
+			if (!strcmp(text, "trans") || !strcmp(text, "transparent") || !strcmp(text, "none"))
+				ctx->trans = 1;
+			else
+			{
+				ctx->trans = 0;
+				ret = text_color(ctx->bgcolor, text);
+				if (ret)
+					goto invbg;
+			}
+		}
+	}
+	if (!ctx->text)
+	{
+		log_log(MODNAME, "No <text> tag defined\n");
+		return -1;
+	}
+	
+	return 0;
+}
+
+static
+int
+text_color(unsigned char *buf, unsigned char *text)
+{
+	int idx, reth, retl;
+	
+	if (!text)
+		return -1;
+	if (*text == '#')
+		text++;
+	if (strlen(text) < 6)
+		return -1;
+	for (idx = 0; idx < 3; idx++)
+	{
+		reth = text_hexdig(*text++);
+		retl = text_hexdig(*text++);
+		if (reth < 0 || retl < 0)
+			return -1;
+		buf[idx] = (reth << 4) | retl;
+	}
+	return 0;
+}
+
+static
+int
+text_hexdig(unsigned char c)
+{
+	if (c >= '0' && c <= '9')
+		return c - '0';
+	else if (c >= 'a' && c <= 'f')
+		return c - 'a' + 10;
+	else if (c >= 'A' && c <= 'F')
+		return c - 'A' + 10;
+	else
+		return -1;
 }
 
