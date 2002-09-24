1d9520de3fc13dba3de9debd44a3e484d47dc55e
dfx <>
2002-09-24 15:27:48 +0000

    added librotate

--- src/image.h
+++ src/image.h
@@ -1,6 +1,8 @@
 #ifndef _IMAGE_H_
 #define _IMAGE_H_
 
+/* $Id$ */
+
 struct image
 {
 	unsigned int x, y;
@@ -9,16 +11,16 @@ struct image
 };
 
 /* Inits image struct and allocates buf */
-void image_new(struct image *, unsigned int, unsigned int);
+void image_new(struct image *, unsigned int x, unsigned int y);
 
 /* Copies image with contents (= image_dup & memcpy(buf, buf)) */
-void image_copy(struct image *, const struct image *);
+void image_copy(struct image *dest, const struct image *src);
 
 /* Only duplicates the structure and buffer, not the contents of the buffer */
-void image_dup(struct image *, const struct image *);
+void image_dup(struct image *dest, const struct image *src);
 
 /* Moves the buffer and structure over to another var, and frees the old var and buf */
-void image_move(struct image *, const struct image *);
+void image_move(struct image *dest, const struct image *src);
 
 /* Frees buffer */
 void image_destroy(struct image *);
