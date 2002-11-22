fe377268d3da8dcfd821596e19914a0c927f9a95
dfx <>
2002-11-22 16:24:31 +0000

    mmap v4l api support

--- src/grab.c
+++ src/grab.c
@@ -7,6 +7,8 @@
 #include <dlfcn.h>
 #include <libxml/parser.h>
 #include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 
 #include "config.h"
 
@@ -99,6 +101,11 @@ grab_thread(void *arg)
 	unsigned int bpf;
 	unsigned char *imgbuf;
 	struct image newimg;
+	int usemmap;
+	struct video_mbuf mbuf;
+	unsigned char *mbufp;
+	struct video_mmap mmapctx;
+	int mbufframe;
 	
 	memcpy(&camdev, arg, sizeof(camdev));
 	free(arg);
@@ -107,6 +114,9 @@ grab_thread(void *arg)
 	imgbuf = malloc(bpf);
 	
 	printf("Camsource " VERSION " ready to grab images...\n");
+	usemmap = -1;
+	mbufp = NULL;
+	mbufframe = 0;
 	for (;;)
 	{
 		pthread_mutex_lock(&current_img.mutex);
@@ -114,9 +124,64 @@ grab_thread(void *arg)
 			pthread_cond_wait(&current_img.request_cond, &current_img.mutex);
 		pthread_mutex_unlock(&current_img.mutex);
 		
-		ret = read(camdev.fd, imgbuf, bpf);
 		image_new(&newimg, camdev.x, camdev.y);
-		camdev.pal->routine(&newimg, imgbuf);
+		if (usemmap)
+		{
+			if (usemmap < 0)
+			{
+				ret = ioctl(camdev.fd, VIDIOCGMBUF, &mbuf);
+				if (ret < 0)
+				{
+nommap:
+					printf("Not using mmap interface, falling back to read()\n");
+					usemmap = 0;
+					goto sysread;
+				}
+				mbufp = mmap(NULL, mbuf.size, PROT_READ, MAP_PRIVATE, camdev.fd, 0);
+				if (mbufp == MAP_FAILED)
+					goto nommap;
+				usemmap = 1;
+				mbufframe = 0;
+			}
+			memset(&mmapctx, 0, sizeof(mmapctx));
+			mmapctx.frame = mbufframe;
+			mmapctx.width = camdev.x;
+			mmapctx.height = camdev.y;
+			mmapctx.format = camdev.pal->val;
+			ret = ioctl(camdev.fd, VIDIOCMCAPTURE, &mmapctx);
+			if (ret < 0)
+			{
+unmap:
+				munmap(mbufp, mbuf.size);
+				goto nommap;
+			}
+			ret = mbufframe;
+			ret = ioctl(camdev.fd, VIDIOCSYNC, &ret);
+			if (ret < 0)
+				goto unmap;
+			camdev.pal->routine(&newimg, mbufp + mbuf.offsets[mbufframe]);
+			mbufframe++;
+			if (mbufframe >= mbuf.frames)
+				mbufframe = 0;
+		}
+		else
+		{
+sysread:
+			ret = read(camdev.fd, imgbuf, bpf);
+			if (ret < 0)
+			{
+				printf("Error while reading from device: %s\n", strerror(errno));
+				exit(1);
+			}
+			if (ret == 0)
+			{
+				printf("EOF while reading from device\n");
+				exit(1);
+			}
+			if (ret < bpf)
+				printf("Short read while reading from device (%i < %i), continuing anyway\n", ret, bpf);
+			camdev.pal->routine(&newimg, imgbuf);
+		}
 		
 		grab_glob_filters(&newimg);
 
